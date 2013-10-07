e600db9e388c9d6ed11b5990e2cb12c345e5e43a
lighthart <dlighthart@mesd.k12.or.us>
2013-10-07 11:26:34 -0700

    Skelpath fix for 2.3

--- Command/MESDControllerGenerator.php
+++ Command/MESDControllerGenerator.php
@@ -38,45 +38,43 @@ class MESDControllerGenerator extends Generator
      * Constructor.
      *
      * @param Filesystem $filesystem  A Filesystem instance
-     * @param string     $skeletonDir Path to the skeleton directory
+     * @param string  $skeletonDir Path to the skeleton directory
      */
-    public function __construct(Filesystem $filesystem, $skeletonDir)
-    {
+    public function __construct( Filesystem $filesystem, $skeletonDir ) {
         $this->filesystem  = $filesystem;
-        $this->skeletonDir = $skeletonDir;
+        $this->setSkeletonDirs($skeletonDir);
+        // $this->skeletonDir = $skeletonDir;
     }
 
     /**
      * Generate the CRUD controller.
      *
-     * @param BundleInterface   $bundle           A bundle object
-     * @param string            $entity           The entity relative class name
+     * @param BundleInterface $bundle           A bundle object
+     * @param string  $entity           The entity relative class name
      * @param ClassMetadataInfo $metadata         The entity class metadata
-     * @param string            $format           The configuration format (xml, yaml, annotation)
-     * @param string            $routePrefix      The route name prefix
-     * @param array             $needWriteActions Wether or not to generate write actions
+     * @param string  $format           The configuration format (xml, yaml, annotation)
+     * @param string  $routePrefix      The route name prefix
+     * @param array   $needWriteActions Wether or not to generate write actions
      *
      * @throws \RuntimeException
      */
-    public function generate(BundleInterface $bundle, $entity, ClassMetadataInfo $metadata, $format, $routePrefix, $noWriteActions, $overwrite)
-    {
+    public function generate( BundleInterface $bundle, $entity, ClassMetadataInfo $metadata, $format, $routePrefix, $noWriteActions, $overwrite ) {
         $this->routePrefix = $routePrefix;
-        $this->routeNamePrefix = str_replace('/', '_', $routePrefix);
-        $this->actions = $noWriteActions ? array('index', 'show') : array('index', 'show', 'new', 'edit', 'delete') ;
+        $this->routeNamePrefix = str_replace( '/', '_', $routePrefix );
+        $this->actions = $noWriteActions ? array( 'index', 'show' ) : array( 'index', 'show', 'new', 'edit', 'delete' ) ;
         $this->overwrite=$overwrite;
-
-        if (count($metadata->identifier) > 1) {
-            throw new \RuntimeException('The CRUD generator does not support entity classes with multiple primary keys.');
+        if ( count( $metadata->identifier ) > 1 ) {
+            throw new \RuntimeException( 'The CRUD generator does not support entity classes with multiple primary keys.' );
         }
 
-        if (!in_array('id', $metadata->identifier)) {
-            throw new \RuntimeException('The CRUD generator expects the entity object has a primary key field named "id" with a getId() method.');
+        if ( !in_array( 'id', $metadata->identifier ) ) {
+            throw new \RuntimeException( 'The CRUD generator expects the entity object has a primary key field named "id" with a getId() method.' );
         }
 
         $this->entity   = $entity;
         $this->bundle   = $bundle;
         $this->metadata = $metadata;
-        $this->setFormat($format);
+        $this->setFormat( $format );
 
         $this->generateControllerClass();
 
@@ -93,20 +91,19 @@ class MESDControllerGenerator extends Generator
     /**
      * Sets the configuration format.
      *
-     * @param string $format The configuration format
+     * @param string  $format The configuration format
      */
-    private function setFormat($format)
-    {
-        switch ($format) {
-            case 'yml':
-            case 'xml':
-            case 'php':
-            case 'annotation':
-                $this->format = $format;
-                break;
-            default:
-                $this->format = 'yml';
-                break;
+    private function setFormat( $format ) {
+        switch ( $format ) {
+        case 'yml':
+        case 'xml':
+        case 'php':
+        case 'annotation':
+            $this->format = $format;
+            break;
+        default:
+            $this->format = 'yml';
+            break;
         }
     }
 
@@ -114,86 +111,91 @@ class MESDControllerGenerator extends Generator
      * Generates the routing configuration.
      *
      */
-    private function generateConfiguration()
-    {
-        if (!in_array($this->format, array('yml', 'xml', 'php'))) {
+    private function generateConfiguration() {
+        if ( !in_array( $this->format, array( 'yml', 'xml', 'php' ) ) ) {
             return;
         }
 
         $target = sprintf(
             '%s/Resources/config/routing/%s.%s',
             $this->bundle->getPath(),
-            strtolower(str_replace('\\', '_', $this->entity)),
+            strtolower( str_replace( '\\', '_', $this->entity ) ),
             $this->format
         );
 
-        $this->renderFile($this->skeletonDir, 'config/routing.'.$this->format.'.twig', $target, array(
-            'actions'           => $this->actions,
-            'route_prefix'      => $this->routePrefix,
-            'route_name_prefix' => $this->routeNamePrefix,
-            'bundle'            => $this->bundle->getName(),
-            'entity'            => $this->entity,
-        ));
+        $this->renderFile(
+            'config/routing.'.$this->format.'.twig', $target
+            , array(
+                'actions'           => $this->actions,
+                'route_prefix'      => $this->routePrefix,
+                'route_name_prefix' => $this->routeNamePrefix,
+                'bundle'            => $this->bundle->getName(),
+                'entity'            => $this->entity,
+            )
+        );
     }
 
     /**
      * Generates the controller class only.
      *
      */
-    private function generateControllerClass()
-    {
+    private function generateControllerClass() {
         $dir = $this->bundle->getPath();
-
-        $parts = explode('\\', $this->entity);
-        $entityClass = array_pop($parts);
-        $entityNamespace = implode('\\', $parts);
+        $parts = explode( '\\', $this->entity );
+        $entityClass = array_pop( $parts );
+        $entityNamespace = implode( '\\', $parts );
 
         $target = sprintf(
             '%s/Controller/%s/%sController.php',
             $dir,
-            str_replace('\\', '/', $entityNamespace),
+            str_replace( '\\', '/', $entityNamespace ),
             $entityClass
         );
 
-        if (file_exists($target) && !$this->overwrite) {
-            throw new \RuntimeException('Unable to generate the controller as it already exists.');
+        if ( file_exists( $target ) && !$this->overwrite ) {
+            throw new \RuntimeException( 'Unable to generate the controller as it already exists.' );
         }
 
-        $this->renderFile($this->skeletonDir, 'controller/controller.php.twig', $target, array(
-            'actions'           => $this->actions,
-            'route_prefix'      => $this->routePrefix,
-            'route_name_prefix' => $this->routeNamePrefix,
-            'dir'               => $this->skeletonDir,
-            'bundle'            => $this->bundle->getName(),
-            'entity'            => $this->entity,
-            'entity_class'      => $entityClass,
-            'namespace'         => $this->bundle->getNamespace(),
-            'entity_namespace'  => $entityNamespace,
-            'format'            => $this->format,
-        ));
+        $this->renderFile(
+            'controller/controller.php.twig'
+            , $target
+            , array(
+                'actions'           => $this->actions,
+                'route_prefix'      => $this->routePrefix,
+                'route_name_prefix' => $this->routeNamePrefix,
+                'dir'               => $this->skeletonDir,
+                'bundle'            => $this->bundle->getName(),
+                'entity'            => $this->entity,
+                'entity_class'      => $entityClass,
+                'namespace'         => $this->bundle->getNamespace(),
+                'entity_namespace'  => $entityNamespace,
+                'format'            => $this->format,
+            )
+        );
     }
 
     /**
      * Generates the functional test class only.
      *
      */
-    private function generateTestClass()
-    {
-        $parts = explode('\\', $this->entity);
-        $entityClass = array_pop($parts);
-        $entityNamespace = implode('\\', $parts);
+    private function generateTestClass() {
+        $parts = explode( '\\', $this->entity );
+        $entityClass = array_pop( $parts );
+        $entityNamespace = implode( '\\', $parts );
 
         $dir    = $this->bundle->getPath() .'/Tests/Controller';
-        $target = $dir .'/'. str_replace('\\', '/', $entityNamespace).'/'. $entityClass .'ControllerTest.php';
-        $this->renderFile($this->skeletonDir, 'tests/controllerTest.php.twig', $target, array(
-            'route_prefix'      => $this->routePrefix,
-            'route_name_prefix' => $this->routeNamePrefix,
-            'entity'            => $this->entity,
-            'entity_class'      => $entityClass,
-            'namespace'         => $this->bundle->getNamespace(),
-            'entity_namespace'  => $entityNamespace,
-            'actions'           => $this->actions,
-            'dir'               => $this->skeletonDir,
-        ));
+        $target = $dir .'/'. str_replace( '\\', '/', $entityNamespace ).'/'. $entityClass .'ControllerTest.php';
+        $this->renderFile(
+            'tests/controllerTest.php.twig'
+            , $target, array(
+                'route_prefix'      => $this->routePrefix,
+                'route_name_prefix' => $this->routeNamePrefix,
+                'entity'            => $this->entity,
+                'entity_class'      => $entityClass,
+                'namespace'         => $this->bundle->getNamespace(),
+                'entity_namespace'  => $entityNamespace,
+                'actions'           => $this->actions,
+                'dir'               => $this->skeletonDir,
+            ) );
     }
 }
